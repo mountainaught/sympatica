9bfd42f41282376553e3759a1044a02c43db303b
mountainaught <tibetunal1@gmail.com>
2025-11-10 13:26:57 +0000

    redo folder structure/fix broken stuff

--- sympatica/api/urls.py
+++ sympatica/api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sympatica project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/stable/topics/http/urls/
